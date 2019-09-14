0eee94d15b0bdf5d5078109653d1d02f96680c5a
Frank Hunleth <fhunleth@troodon-software.com>
2019-09-14 16:17:09 -0400

    Add docs, dialyzer, and package info

--- mix.exs
+++ mix.exs
@@ -1,29 +1,59 @@
 defmodule CircuitsLED.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+  @source_url "https://github.com/elixir-circuits/circuits_led"
+
   def project do
     [
       app: :circuits_led,
-      version: "0.1.0",
-      elixir: "~> 1.8",
+      version: @version,
+      elixir: "~> 1.7",
+      description: description(),
+      package: package(),
+      source_url: @source_url,
+      docs: docs(),
       start_permanent: Mix.env() == :prod,
+      dialyzer: [
+        flags: [:unmatched_returns, :error_handling, :race_conditions, :underspecs]
+      ],
       deps: deps()
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
-  def application do
+  def application, do: []
+
+  defp description do
+    "Use LEDs in Elixir"
+  end
+
+  defp package do
+    %{
+      files: [
+        "lib",
+        "test",
+        "mix.exs",
+        "README.md",
+        "LICENSE"
+      ],
+      licenses: ["Apache-2.0"],
+      links: %{"GitHub" => @source_url}
+    }
+  end
+
+  defp deps do
     [
-      extra_applications: [:logger],
-      mod: {CircuitsLED.Application, []}
+      {:ex_doc, "~> 0.11", only: :docs, runtime: false},
+      {:dialyxir, "~> 1.0.0-rc.6", only: :dev, runtime: false}
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
-  defp deps do
+  defp docs do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      extras: ["README.md"],
+      main: "readme",
+      source_ref: "v#{@version}",
+      source_url: @source_url
     ]
   end
 end
